3d4ebbb2a59d3e893a6353e203161993e9c232c5
juazocar <ju.azocar@profesor.duoc.cl>
2023-05-25 21:53:30 -0400

    COMMIT

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, form_vehiculo
 
 urlpatterns = [
     path('', home, name="home"),
+    path('form-vehiculo', form_vehiculo, name="form_vehiculo"),
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Vehiculo
+from .forms import VehiculoForm
 
 # Create your views here.
 
@@ -9,3 +10,18 @@ def home(request):
         'vehiculos': vehiculos
     }
     return render(request, 'core/home.html', datos)
+
+def form_vehiculo(request):
+
+    datos = {
+        'form': VehiculoForm()
+    }
+
+    if request.method == 'POST':
+        formulario = VehiculoForm(request.POST)
+
+        if formulario.is_valid:
+            formulario.save()
+
+            datos['mensaje'] = "Datos guardados correctamente."
+    return render(request, 'core/form_vehiculo.html', datos)    
